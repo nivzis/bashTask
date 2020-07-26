cfdb78c4d82089d8729f6412b29d35cc78b25b3f
nivzis <40835359+nivzis@users.noreply.github.com>
2020-07-26 08:58:57 +0300

    Update FirstProj.bash

--- FirstProj.bash
+++ FirstProj.bash
@@ -1,9 +1,20 @@
 File=$1
 Action=$2
-if [ $2 = "count" ]; then
-	wc -m < $1
-fi
-if [ $2 = "invert" ]; then
-	rev $1
-fi
 
+if [[ -f $FILE ]]; then
+
+	if [ $2 = "count" ]; then
+	wc -m < $1
+	fi
+	if [ $2 = "invert" ]; then
+	rev $1
+	fi
+else
+	content=$(curl -L $1)
+	if [ $2 = "count" ]; then
+	wc -m < content
+	fi
+	if [ $2 = "invert" ]; then
+	rev content
+	fi
+fi
